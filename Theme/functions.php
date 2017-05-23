27878dd4a71b0d1fa7553ee5cb0fb6d5cba52e9b
fjzusi <zidane320@yahoo.com>
2017-05-23 14:43:11 -0400

    Added vendor files

--- Theme/functions.php
+++ Theme/functions.php
@@ -105,11 +105,16 @@ add_action( 'widgets_init', 'novel_dragon_widgets_init' );
  * Enqueue scripts and styles.
  */
 function novel_dragon_scripts() {
+  wp_enqueue_style( 'novel-dragon-bootstrap', get_template_directory_uri() . '/css/vendor/bootstrap.min.css', array(), '20151215', true );
+  wp_enqueue_style( 'novel-dragon-bootstrap-theme', get_template_directory_uri() . '/css/vendor/bootstrap-theme.min.css', array(), '20151215', true );
+  wp_enqueue_style( 'novel-dragon-bootstrap-theme', get_template_directory_uri() . '/css/vendor/normalize.css', array(), '20151215', true );
 	wp_enqueue_style( 'novel-dragon-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'novel-dragon-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'novel-dragon-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'novel-dragon-modernizer', get_template_directory_uri() . '/js/vendor/modernizr-2.8.3.min.js', array(), '20151215', true );
+	wp_enqueue_script( 'novel-dragon-jquery', get_template_directory_uri() . '/js/vendor/jquery-3.2.0.min.js', array(), '20151215', true );
+  wp_enqueue_script( 'novel-dragon-bootstrap', get_template_directory_uri() . '/js/vendor/bootstrap.min.js', array(), '20151215', true );
+  wp_enqueue_script( 'novel-dragon-plugins', get_template_directory_uri() . '/js/plugins.js', array(), '20151215', true );
+  wp_enqueue_script( 'novel-dragon-main', get_template_directory_uri() . '/js/main.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
